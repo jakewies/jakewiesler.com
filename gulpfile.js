1881e40a619f809d8eaf475dc52769fb9901366d
Jake Wiesler <jakewiesler@gmail.com>
2017-09-19 22:17:11 -0400

    Add autoprefixer support using the gulp-autoprefixer plugin.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp')
 var stylus = require('gulp-stylus')
 var sourcemaps = require('gulp-sourcemaps')
+var autoprefixer = require('gulp-autoprefixer')
 
 gulp.task('styles', function() {
   return gulp
     .src('src/styles/*.styl')
     .pipe(sourcemaps.init())
     .pipe(stylus())
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions']
+      })
+    )
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('static/css'))
 })
